404fc7d0396d2b930ddb6fd75325d227e92c243d
Magdaleno <lmagdaleno@dusd.net>
2018-10-08 11:28:15 -0700

    arun(host='127.0.0.1', debug=True, port='5000') Update app.py imported render_template changed index() from returning a string to returning rendered index.html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello'
+    return render_template('index.html')
 
 app.run(host='127.0.0.1', debug=True, port='5000')
